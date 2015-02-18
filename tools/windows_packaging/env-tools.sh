45947f3de68c00193d295a4c49dd3586b5866536
Tim Mayberry <mojofunk@gmail.com>
2015-02-18 20:54:03 +1000

    When export tool env vars set defaults if vars not already set

--- tools/windows_packaging/env-tools.sh
+++ tools/windows_packaging/env-tools.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
+# export necessary env vars, setting defaults if not already set.
 
-export PKG_CONFIG_PREFIX=$MINGW_ROOT
-export PKG_CONFIG_LIBDIR=$MINGW_ROOT/lib/pkgconfig
-export PKGCONFIG=pkg-config
-export AR=$HOST-ar
-export RANLIB=$HOST-ranlib
-export CC=$HOST-gcc
-export CPP=$HOST-g++
-export CXX=$HOST-g++
-export AS=$HOST-as
-export LINK_CC=$HOST-gcc
-export LINK_CXX=$HOST-g++
-export WINRC=$HOST-windres
-export STRIP=$HOST-strip
-export PYTHON=python
+export PKG_CONFIG_PREFIX=${PKG_CONFIG_PREFIX:=$MINGW_ROOT}
+export PKG_CONFIG_LIBDIR=${PKG_CONFIG_LIBDIR:=$MINGW_ROOT/lib/pkgconfig}
+export PKGCONFIG=${PKGCONFIG:=pkg-config}
+export AR=${AR:=$HOST-ar}
+export RANLIB=${RANLIB:=$HOST-ranlib}
+export CC=${CC:=$HOST-gcc}
+export CPP=${CPP:=$HOST-g++}
+export CXX=${CXX:=$HOST-g++}
+export AS=${AS:=$HOST-as}
+export LINK_CC=${LINK_CC:=$HOST-gcc}
+export LINK_CXX=${LINK_CXX:=$HOST-g++}
+export WINRC=${WINRC:=$HOST-windres}
+export STRIP=${STRIP:=$HOST-strip}
+export PYTHON=${PYTHON:=python}

b03ed4026d007fbec014fdc95b95a797c28be824
Tim Mayberry <mojofunk@gmail.com>
2015-02-24 13:19:40 +1000

    Add --program-name=${PROGRAM_NAME} to common configure options

--- tools/windows_packaging/env-options.sh
+++ tools/windows_packaging/env-options.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-COMMON_OPTIONS=${COMMON_OPTIONS:="--noconfirm --use-external-libs --dist-target=mingw"}
+COMMON_OPTIONS=${COMMON_OPTIONS:="--noconfirm --use-external-libs --dist-target=mingw --program-name=${PROGRAM_NAME}"}
 PATH_OPTIONS=${PATH_OPTIONS:="--prefix=/ --configdir=/share"}
 TEST_OPTIONS=${TEST_OPTIONS:="--test --single-tests"}
 BACKEND_OPTIONS=${BACKEND_OPTIONS:="--with-backends=dummy,jack"}

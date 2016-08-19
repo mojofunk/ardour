1ef0441e6ca620757396ecf876d29ae854dbb31b
Tim Mayberry <mojofunk@gmail.com>
2016-08-19 21:40:53 +1000

    Use ID::to_s() in libardour instead of ID::print()

--- libs/ardour/route.cc
+++ libs/ardour/route.cc
@@ -2233,8 +2233,7 @@ Route::state(bool full_state)
 	ProcessorList::iterator i;
 	char buf[32];
 
-	id().print (buf, sizeof (buf));
-	node->add_property("id", buf);
+	node->add_property ("id", id ().to_s ());
 	node->add_property ("name", _name);
 	node->add_property("default-type", _default_type.to_string());
 	node->add_property ("strict-io", _strict_io);
@@ -2308,8 +2307,7 @@ Route::state(bool full_state)
 	if (_custom_meter_position_noted) {
 		boost::shared_ptr<Processor> after = _processor_after_last_custom_meter.lock ();
 		if (after) {
-			after->id().print (buf, sizeof (buf));
-			node->add_property (X_("processor-after-last-custom-meter"), buf);
+			node->add_property (X_("processor-after-last-custom-meter"), after->id().to_s());
 		}
 	}
 

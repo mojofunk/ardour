abd878227969fc2150a91c2e1c54ae0d2a3e6c03
Tim Mayberry <mojofunk@gmail.com>
2016-08-29 18:11:04 +1000

    Use XMLNode::get/set_property API in LocationUI class

--- gtk2_ardour/location_ui.cc
+++ gtk2_ardour/location_ui.cc
@@ -27,6 +27,7 @@
 
 #include "ardour_ui.h"
 #include "clock_group.h"
+#include "enums_convert.h"
 #include "main_clock.h"
 #include "gui_thread.h"
 #include "keyboard.h"
@@ -1140,7 +1141,7 @@ XMLNode &
 LocationUI::get_state () const
 {
 	XMLNode* node = new XMLNode (X_("LocationUI"));
-	node->add_property (X_("clock-mode"), enum_2_string (_clock_group->clock_mode ()));
+	node->set_property (X_("clock-mode"), _clock_group->clock_mode ());
 	return *node;
 }
 
@@ -1152,12 +1153,12 @@ LocationUI::clock_mode_from_session_instant_xml () const
 		return AudioClock::Frames;
 	}
 
-	XMLProperty const * p = node->property (X_("clock-mode"));
-	if (!p) {
+	AudioClock::Mode mode;
+	if (!node->get_property (X_("clock-mode"), mode)) {
 		return ARDOUR_UI::instance()->secondary_clock->mode();
 	}
 
-	return (AudioClock::Mode) string_2_enum (p->value (), AudioClock::Mode);
+	return mode;
 }
 
 
